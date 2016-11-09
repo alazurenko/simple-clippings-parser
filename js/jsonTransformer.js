ac86a670c6830f7c841d86c75a71911224c72e6c
Andrii Lazurenko <andrey.lazurenko@gmail.com>
2016-11-09 22:07:44 +0100

    Files tweaks

---
+++ js/jsonTransformer.js
@@ -0,0 +1,29 @@
+'use strict';
+function transform(paragraph) {
+  // paragraph = [0] headings; [1] date details; [2] cite
+
+  return Object.assign(
+        getBookDetails(paragraph[0]),
+        getTimeDetails(paragraph[1]),
+        { quote: paragraph[2] }
+      );
+}
+
+function getBookDetails(heading) {
+  let head = heading.split(/[()]/);
+
+  return {
+    book: head[0] && head[0].trim(),
+    author: head[1] && head[1].trim()
+  };
+}
+
+function getTimeDetails(time) {
+  let dateOfCite = time.split('|').pop();
+
+  return {
+    date: new Date(dateOfCite.trim())
+  };
+}
+
+module.exports = transform;
---
+++ js/quoteParser.js
@@ -0,0 +1,28 @@
+'use strict';
+const jsonTransformer = require('./jsonTransformer');
+const SEPARATOR = '==========';
+
+function parseQuotes(data) {
+  let readData = data.split(SEPARATOR);
+  readData.pop(); //remove last empty quote due to separator
+
+  return readData.map(parseQuote);
+}
+
+function parseQuote(quote) {
+  let paragraphs = quote.split('\n')
+                        .filter(filterParagraphs)
+                        .map(prepareParagraph);
+
+  return jsonTransformer(paragraphs);
+}
+
+function filterParagraphs(paragraph) {
+  return paragraph !== '\r' && paragraph !== '';
+}
+
+function prepareParagraph(paragraph) {
+  return paragraph.replace('\r', '');
+}
+
+module.exports = parseQuotes;
